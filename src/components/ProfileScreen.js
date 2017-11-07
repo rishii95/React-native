e9910207c0b0ecfaa60d75821364b524fbd47ea9
Rishi Agarwal <rishiagarwal95@gmail.com>
2017-11-07 20:35:26 +0530

    Added push notification

--- src/components/ProfileScreen.js
+++ src/components/ProfileScreen.js
@@ -20,6 +20,7 @@ import ReversedFlatList from 'react-native-reversed-flat-list';
 
 import {send, subscribe} from 'react-native-training-chat-server';
 
+import { Permissions, Notifications } from 'expo';
 
 
 
@@ -31,8 +32,12 @@ function mapStateToProps(state, props) {
 }
 function mapDispatchToProps(dispatch){return bindActionCreators(Actions,dispatch);}
 
+
+const PUSH_ENDPOINT = 'https://exp.host/--/api/v2/push/send';
+
+
 // const NAME = 'sdd';
-const CHANNEL = 'Rishi';
+const CHANNEL = 'HElloo';
 const AVATAR =
   'https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg';
 
@@ -44,12 +49,68 @@ const AVATAR =
     state = {
       typing: '',
       messages: [],
+      token:[],
+      notification:{}
     };
     componentWillMount() {
       subscribe(CHANNEL, messages => {
         this.setState({messages});
       });
+      this.registerForPushNotificationsAsync();
+            if(this.props.userName=='rishi')
+            this.setState({token:"ExponentPushToken[DzXktGC92UX5bLHI3h_VYi]"})
+            else
+            this.setState({token:"ExponentPushToken[6lB3SgEv-mS0RJ7wwGPodh]"})
+            
+          }
+    registerForPushNotificationsAsync= async ()=> {
+      const { status: existingStatus } = await Permissions.getAsync(
+        Permissions.NOTIFICATIONS
+      );
+      let finalStatus = existingStatus;
+    
+      // only ask if permissions have not already been determined, because
+      // iOS won't necessarily prompt the user a second time.
+      if (existingStatus !== 'granted') {
+        // Android remote notification permissions are granted during the app
+        // install, so this will only ask on iOS
+        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        finalStatus = status;
+      }
+    
+      // Stop here if the user did not grant permissions
+      if (finalStatus !== 'granted') {
+        return;
+      }
+    
+      // Get the token that uniquely identifies this device
+      let token = await Notifications.getExpoPushTokenAsync();
+      // POST the token to your backend server from where you can retrieve it to send push notifications.
+      // return fetch(PUSH_ENDPOINT, {
+      //   method: 'POST',
+      //   headers: {
+      //     Accept: 'application/json',
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify({
+      //     token: {
+      //       value: token,
+      //     }, 
+      //     user: {
+      //       username: this.props.userName,
+      //     },
+      //   }),
+      // });
+      console.log("token",token)
+
+    };
+    componentDidMount(){
+      this._notificationSubscription = Notifications.addListener(this._handleNotification);
+      
     }
+    _handleNotification = (notification) => {
+      this.setState({notification: notification});
+    };
     sendMessage = async () => {
       // read message from component state
       const message = this.state.typing;
@@ -65,6 +126,27 @@ const AVATAR =
       // set the component state (clears text input)
       this.setState({
         typing: '',
+      });
+
+      // if(this.props.userName=='rishi')
+      // let token="ExponentPushToken[DzXktGC92UX5bLHI3h_VYi]";
+      // else
+      // let token="ExponentPushToken[6lB3SgEv-mS0RJ7wwGPodh]";
+      
+      fetch(PUSH_ENDPOINT, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'accept-encoding':'gzip, deflate',
+        },
+        body: JSON.stringify(
+          {  
+            to: this.state.token,
+            sound: "default",
+            body: "You have a new message"
+          
+        }),
       });
     };
   
